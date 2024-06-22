5b477164602d81d480f9217f7360d6ffd76416d4
Monkeyshoulder1 <hilkiaboanerges370@gmail.com>
2024-06-22 08:55:52 +0700

    uasku

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,115 @@
+@extends('layouts.app-public')
+@section('title', 'Home')
+@section('content')
+    <div class="site-wrapper-reveal">
+        <div class="hero-box-area">
+            <div class="container">
+                <div class="row ">
+                    <div class="col-lg-12">
+                        <!-- Hero Slider Area Start -->
+                        <div class="hero-area" id="product-preview">
+                        </div>
+                        <!-- Hero Slider Area End -->
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="about-us-area section-space--ptb_120">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="about-us-content_6 text-center">
+                            <h2>SPRING BED&nbsp;&nbsp;Store</h2>
+                            <p>
+                                <small>
+                                Created to provide the ultimate pleasure in sleeping. 
+                                Featuring a soft, comfortable Plush Top and Pillow Top, 
+                                Dacron-ﬁlled quilted cover plus multiple layers of natural latex and memory foam.  
+                                this combination of goodness will bring your deﬁnition of sleep to the next level. &#10084;
+                                </small>
+                            </p>
+                            <p class="mt-5">Find your , <span class="text-color-primary">springbed in the WORLD! Or, even</span> 
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Banner Video Area Start -->
+        <div class="banner-video-area overflow-hidden">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="banner-video-box">
+                            <img src="https://i.ibb.co.com/VNfV5QK/chiro-q.jpg" alt="">
+                            <div class="video-icon">
+                                <a href="https://www.youtube.com/watch?v=5OGAWNjpYag" class="popup-youtube"><i class="linear-ic-play"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Banner Video Area End -->
+
+        <!-- Our Brand Area Start -->
+        <div class="our-brand-area section-space--pb_90">
+            
+        </div>
+        <!-- Our Brand Area End -->
+
+        <!-- Our Member Area Start -->
+        <div class="our-member-area section-space--pb_120">
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="member--box">
+                            <div class="row align-items-center">
+                                <div class="col-lg-5 col-md-4">
+                                    <div class="section-title small-mb__40 tablet-mb__40">
+                                        <h4 class="section-title">Join the community!</h4>
+                                        <p>Become one of the member and get discount 50% off</p>
+                                    </div>
+                                </div>
+                                <div class="col-lg-7 col-md-8">
+                                    <div class="member-wrap">
+                                        <form action="#" class="member--two">
+                                            <input class="input-box" type="text" placeholder="Your email address">
+                                            <button class="submit-btn"><i class="icon-arrow-right"></i></button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Our Member Area End -->
+
+    </div>
+@endsection
+@section('addition_css')
+@endsection
+@section('addition_script')
+    <script src="{{asset('pages/js/home.js')}}"></script>
+@endsection
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
